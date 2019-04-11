6553bc0930ceb809f619618bea5f4073d52c20e2
Maxwell L Rudolph <maxrudolph@ucdavis.edu>
2019-04-11 13:13:02 -0700

    add updated results files

--- convection-3d-shell-test/organize_results.sh
+++ convection-3d-shell-test/organize_results.sh
@@ -3,7 +3,7 @@ mkdir results
 for file in output-*
 do
 echo $file
-j=`echo $file | cut -d _ -f 4`
+j=`echo $file | cut -d _ -f 4-5`
 echo $j
 cp $file/statistics results/statistics-$j
 cp $file/depth_average.txt results/depth_average-$j.txt
