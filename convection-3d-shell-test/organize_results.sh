28920e7405c73564062ad436b29012949ecc7a75
Maxwell L Rudolph <maxrudolph@ucdavis.edu>
2019-04-11 11:34:05 -0700

    add preliminary results and script to organize them

---
+++ convection-3d-shell-test/organize_results.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir results
+for file in output-*
+do
+echo $file
+j=`echo $file | cut -d _ -f 4`
+echo $j
+cp $file/statistics results/statistics-$j
+cp $file/depth_average.txt results/depth_average-$j.txt
+done
