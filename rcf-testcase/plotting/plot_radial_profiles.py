4b604492dcee882ce93d6639b7bb2c46f4b0254d
Max Rudolph <maxwellr@gmail.com>
2019-04-29 11:42:24 -0700

    reduce depth average frequency

--- rcf-testcase/plotting/plot_radial_profiles.py
+++ rcf-testcase/plotting/plot_radial_profiles.py
@@ -34,26 +34,61 @@ for file in files:
     depth_averages.append( load_depthaverage(file) )
 
 
-# CitcomS averages from FESD Case05 final timestep
-# viscosity profile from CitcomS
-citcoms_data = np.loadtxt("citcoms_results/arr.horiz_avg.0.29090")
-citcoms_r = citcoms_data[:,0]
-citcoms_visc_nd = citcoms_data[:,4]
-citcoms_T = citcoms_data[:,1]*2500.
+# load the citcoms time fle
+citcoms_prefix = '../r660-zzs-citcoms/R660_ZZS'
+citcoms_time = np.loadtxt(citcoms_prefix+'.time')
+citcoms_stepnum = citcoms_time[:,0]
+citcoms_tnd = citcoms_time[:,1]
+Rearth = 6.371e6;
+kappa = 1.0e-6;
+seconds_in_Myr = 3.1556926e13;
+dimensional_factor = Rearth**2/kappa/seconds_in_Myr;
+citcoms_time_myr = dimensional_factor * citcoms_tnd
+citcoms_time_yr = dimensional_factor * citcoms_tnd*1.0e6
 
-plt.figure()
-for da in depth_averages:
-    target_time = 1.5e8;
-    times = np.unique(da['time']);
-    ii=np.argmin(np.abs(times-target_time))
-    pmask = da['time'] == times[ii]
+
+def load_citcoms_depthaverage(filename):
+    da = dict()
+    # CitcomS averages from FESD Case05 final timestep
+    # viscosity profile from CitcomS
+    citcoms_data = np.loadtxt(filename)
+    da['depth'] = Rearth*(1.0-citcoms_data[:,0])
+    da['visc'] = citcoms_data[:,4]
+    da['temperature'] = citcoms_data[:,1]*2500.
+    da['filename'] = filename
+    # This pulls out the step number from a citcoms file name
+    da['stepnum'] = int(filename[(filename.rfind('.')+1):])
+    return da
+
+citcoms_files = sorted(glob.glob(citcoms_prefix+'.horiz_avg*'))
+citcoms_da = []
+for file in citcoms_files:
+    citcoms_da.append(load_citcoms_depthaverage(file))
+
+citcoms_file_tyr = []
+for ii in range(len(citcoms_da)):
+    da = citcoms_da[ii]
+    idx = np.argwhere(citcoms_stepnum==da['stepnum'])[0][0]
+    citcoms_file_tyr.append(citcoms_time_yr[idx])
+citcoms_file_tyr = np.array(citcoms_file_tyr)
+
+
+def comparison_figure(target_time):
+    plt.figure()
+    for da in depth_averages:
+        times = np.unique(da['time']);
+        ii=np.argmin(np.abs(times-target_time))
+        pmask = da['time'] == times[ii] 
+        plt.plot( da['temperature'][pmask],da['depth'][pmask] ,label=da['label']+' {:.0f} Myr'.format(times[ii]/1e6))
+    ii = np.argmin(np.abs(citcoms_file_tyr-target_time))
+    plt.plot(citcoms_da[ii]['temperature'],citcoms_da[ii]['depth'],label='citcoms {:.0f} Myr'.format(citcoms_file_tyr[ii]/1e6))
+    plt.legend()
+    plt.ylabel('Depth (m)')
+    plt.xlabel('Temperature (K)')
+    plt.gca().invert_yaxis()
+    plt.show()
     
-    plt.plot( da['temperature'][pmask],da['depth'][pmask] ,label=da['label']+' {:.0f} Myr'.format(times[ii]/1e6))
-
-
-plt.plot(citcoms_T,(1-citcoms_r)*6336000.,':',label='citcoms final')
-plt.ylabel('Depth (m)')
-plt.xlabel('Temperature (K)')
-plt.legend()
-plt.gca().invert_yaxis()
-plt.show()
+comparison_figure(0)
+comparison_figure(0.74aae8)
+comparison_figure(1.5e8)
+comparison_figure(2.5e8)
